a867f8ed8566a7bd3490df7d2bc31fd653317a0d
Rukiye Canlı <canli.rukiye@std.izu.edu.tr>
2024-01-23 17:37:09 +0300

    footer eklendi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 const range = document.querySelector("#range");
 const valueK = document.querySelector(".valueK");
 const price = document.querySelector(".price");
+const secimKutusu = document.querySelector("#secimKutusu");
 
 let prices = [4,8,16,32,64];
 let pageView = ["5K", "50K", "250K", "500K", "1000K"];
 valueK.innerHTML = pageView[0];
 price.innerHTML = prices[0];
+let deger = 0;
+let prices2 = [];
+
 function changeRange(){
 
     // if (range.oninput == 0) {
@@ -22,29 +26,43 @@ function changeRange(){
     // }
 
     range.oninput = function(){
-        if(parseInt(this.value) == 0){
+        let rangeDeger = parseInt(this.value);
+        if(rangeDeger == 0){
             valueK.innerHTML = pageView[0];
-            price.innerHTML = prices[0] + "$ ";
+            changeRange();
+            bindCheckBox(0);
         }
-        if(parseInt(this.value) == 25){
+        if(rangeDeger == 25){
             valueK.innerHTML = pageView[1];
-            price.innerHTML = prices[1] + "$ ";
+            bindCheckBox(1);
         }
-        if(parseInt(this.value) == 50){
+        if(rangeDeger == 50){
             valueK.innerHTML = pageView[2];
-            price.innerHTML = prices[2] + "$ ";
+
+            bindCheckBox(2);
         }
-        if(parseInt(this.value) == 75){
+        if(rangeDeger == 75){
             valueK.innerHTML = pageView[3];
-            price.innerHTML = prices[3] + "$ ";
+            bindCheckBox(3);
         }
-        if(parseInt(this.value) == 100){
+        if(rangeDeger == 100){
             valueK.innerHTML = pageView[4];
-            price.innerHTML = prices[4] + "$ ";
+            bindCheckBox(4);
         }
 
     }
+
     
 }
 
+
+function bindCheckBox(deger){
+    if (secimKutusu.checked == true) {
+        prices2[deger] = prices[deger]*25/100;
+        price.innerHTML = prices2[deger] + "$ ";
+    }
+    else{
+        price.innerHTML = prices[deger] + "$ ";
+    }
+}
 changeRange();
